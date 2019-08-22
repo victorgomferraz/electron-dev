ccec1fca5f4a0729655f9e0ee9b327d5ef756b7e
Victor Gomide Ferraz <victor.vgf@ecxus.com.br>
2019-08-22 16:40:31 -0300

    initial commit

---
+++ electron.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+require('electron-debug');
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+const IP = process.env.IP;
+const PORT = process.env.PORT;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({width: 900, height: 680, fullscreen: true});
+  mainWindow.loadURL(`http://${IP}:${PORT}`);
+  mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
